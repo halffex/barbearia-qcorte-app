350fe9ed2d3a00d3d02dc884ca14df9984effc5d
Heitor Luciano <heitorv44@gmail.com>
2023-06-01 10:42:06 -0300

    update routes arquivo index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,11 +10,12 @@ export default function Routes() {
 
   return (
     <Stack.Navigator>
+      <Stack.Screen name="Perfil" component={Perfil} options={{headerShown : false}} />
+
       <Stack.Screen name="Login" component={Login} options={{headerShown : false}} />
 
       <Stack.Screen name="Cadastro" component={Cadastro} options={{headerShown : false}} />
-
-      <Stack.Screen name="Perfil" component={Perfil} options={{headerShown : false}} />
+      
     </Stack.Navigator>
   );
 }
